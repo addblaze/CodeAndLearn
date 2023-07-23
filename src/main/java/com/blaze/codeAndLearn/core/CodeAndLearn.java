bd85b27b39604fd49fa3a66dc54628b64d762fa5
Atmaram Dhuri <addmin.blaze@gmail.com>
2023-07-23 21:43:30 +0530

    Changes to main class and Logger

--- src/main/java/com/blaze/codeAndLearn/core/CodeAndLearn.java
+++ src/main/java/com/blaze/codeAndLearn/core/CodeAndLearn.java
@@ -2,6 +2,7 @@ package com.blaze.codeAndLearn.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -9,10 +10,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 public class CodeAndLearn {
 	
-	Logger LOGGER = LoggerFactory.getLogger(CodeAndLearn.class);
+	private static Logger LOGGER = LoggerFactory.getLogger("CodeAndLearn");
 	
 	public static void main(String[] args) {
-		
+		LOGGER.info("Running CodeAndLearn App....!");
+		SpringApplication.run(CodeAndLearn.class, args);
 	}
 
 }
