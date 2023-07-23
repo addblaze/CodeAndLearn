f6d26015ffcc01e1aa51632455f7341e764b404d
Atmaram Dhuri <addmin.blaze@gmail.com>
2023-07-23 19:45:32 +0530

    Initial Commit

---
+++ src/main/java/com/blaze/codeAndLearn/core/CodeAndLearn.java
@@ -0,0 +1,18 @@
+package com.blaze.codeAndLearn.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class CodeAndLearn {
+	
+	Logger LOGGER = LoggerFactory.getLogger(CodeAndLearn.class);
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
